a2be17202e02f7cf057b3c864dd1ad5ff6f1c1a2
Macedo1634 <macedo1634@gmail.com>
2018-11-27 19:49:29 +0000

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -434,3 +434,17 @@ $(document).ready(function($) {
 
     }
 });
+
+function openGroup(evt, grupo) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(grupo).style.display = "block";
+    evt.currentTarget.className += " active";
+}

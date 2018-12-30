42016ae79422f22194d52bc2ec9a0aa9d6cb79d3
Macedo1634 <macedo1634@gmail.com>
2018-12-30 21:30:03 +0000

    Create config.php

---
+++ config.php
@@ -0,0 +1,29 @@
+<?php
+
+/** O nome do banco de dados*/
+define('DB_NAME', 'juntadepardilho');
+
+/** Usuário do banco de dados MySQL */
+define('DB_USER', 'root');
+
+/** Senha do banco de dados MySQL */
+define('DB_PASSWORD', '');
+
+/** nome do host do MySQL */
+define('DB_HOST', 'localhost');
+
+/** caminho absoluto para a pasta do sistema **/
+if ( !defined('ABSPATH') )
+    define('ABSPATH', dirname(__FILE__) . '/');
+    
+/** caminho no server para o sistema **/
+if ( !defined('BASEURL') )
+    define('BASEURL', '/');
+    
+/** caminho do arquivo de banco de dados **/
+if ( !defined('DBAPI') )
+    define('DBAPI', ABSPATH . 'agenda/database-agenda.php');
+
+/** caminho do arquivo de banco de dados **/
+if ( !defined('DBAPI2') )
+    define('DBAPI2', ABSPATH . 'noticias/database-noticia.php');

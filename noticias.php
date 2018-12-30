bcefb62924c37f18acfc217c796c237b9d6c78c1
Macedo1634 <macedo1634@gmail.com>
2018-12-30 21:30:14 +0000

    Create noticias.php

---
+++ noticias.php
@@ -0,0 +1,341 @@
+<!DOCTYPE html>
+
+<html lang="en">
+    <head>
+
+        <link rel="icon" href="assets/img/BrasaoPardilho_favIcon.png" type="image/png">
+
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+        <title>Junta de Freguesia de Pardilhó</title>
+
+        <!-- Bootstrap -->
+        <link rel="stylesheet" href="assets/css/bootstrap/bootstrap.min.css">
+
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="assets/css/bootstrap/bootstrap-theme.min.css">
+
+        <!-- Custom css -->
+        <link rel="stylesheet" href="assets/css/style.css">
+        
+        <!-- Font Awesome -->
+        <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+        
+        <link rel="stylesheet" href="assets/css/ionicons.min.css">
+        
+        <!-- Flexslider -->
+        <link rel="stylesheet" href="assets/css/flexslider.css">
+        
+        <!-- Owl -->
+        <link rel="stylesheet" href="assets/css/owl.carousel.css">
+        
+        <!-- Magnific Popup -->
+        <link rel="stylesheet" href="assets/css/magnific-popup.css">
+
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+        <script src="assets/js/show-hide-text.js"></script>
+
+      
+        <!--  loader  -->
+        <div id="myloader">
+            <div class="loader">
+                <div class="grid">
+                    <div class="cube cube1"></div>
+                    <div class="cube cube2"></div>
+                    <div class="cube cube3"></div>
+                    <div class="cube cube4"></div>
+                    <div class="cube cube5"></div>
+                    <div class="cube cube6"></div>
+                    <div class="cube cube7"></div>
+                    <div class="cube cube8"></div>
+                    <div class="cube cube9"></div>
+                </div>
+            </div>
+        </div>
+        
+        <!--  Header & Menu  -->
+        <header id="header">
+            <div class="top-nav">
+                <!--  Header Logo  -->
+                <div id="logo">
+                    <a class="navbar-brand" href="index.html">
+                        <img src="assets/img/BrasaoPardilho_nav.png" class="normal" alt="logo">
+                        <img src="assets/img/logo@2x.png" class="retina" alt="logo">
+                    </a>
+                </div>
+                <!--  END Header Logo  -->
+                <div class="secondary-menu">
+                    <ul>
+                        <li class="mail"><i class="fa fa-envelope" aria-hidden="true"></i><a href="maito:geral@jf-pardilho.com">geral@jf-pardilho.com</a></li>
+                        <li class="phone"><i class="fa fa-phone" aria-hidden="true"></i>(+351) 234 287 211</li>
+                    </ul>
+                </div>
+            </div>
+            <nav class="navbar navbar-default">
+                <!--  Classic menu, responsive menu classic  -->
+                <div id="menu-classic">
+                    <div class="menu-holder">
+                        <ul>
+                            <li>
+                                <a href="index.html">Inicio</a>
+                            </li>
+                            <li class="submenu">
+                                <a href="javascript:void(0)">Freguesia</a>
+                                <ul class="sub-menu">
+                                    <li><a href="historia.html">História</a></li>
+                                    <li><a href="heraldica.html">Heráldica</a></li>
+                                    <li><a href="demografia.html">Demografia</a></li>
+                                    <li><a href="coletividades.html">Coletividades</a></li>
+                                    <li><a href="talento.html">Talento das nossas gentes</a></li>
+                                    <li><a href="executivo.html">Executivo</a></li>
+                                    <li><a href="assembleia.html">Assembleia</a></li>
+                                </ul>
+                            </li>
+                            <li class="submenu">
+                                <a href="javascript:void(0)">Atualidade</a>
+                                <ul class="sub-menu">
+                                    <li><a href="noticias.html">Notícias</a></li>
+                                    <li><a href="#">Agenda</a></li>
+                                </ul>
+                            </li>
+                            <li class="submenu">
+                                <a href="javascript:void(0)">Serviços</a>
+                                <ul class="sub-menu">
+                                    <li><a href="ctt.html">CTT</a></li>
+                                    <li><a href="polointernet.html">Polo Internet</a></li>
+                                    <li><a href="guiche.html">Guiché Online</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="#">Portal</a>
+                            </li>
+                            <li>
+                                <a href="#">Turismo</a>
+                            </li>
+                            <li>
+                                <a href="#">Contactos</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <!--  END Classic menu, responsive menu classic  -->
+                <!--  Button for Responsive Menu Classic  -->
+                <div id="menu-responsive-classic">
+                    <div class="menu-button">
+                        <span class="bar bar-1"></span>
+                        <span class="bar bar-2"></span>
+                        <span class="bar bar-3"></span>
+                    </div>
+                </div>
+                <!--  END Button for Responsive Menu Classic  -->
+                <div class="secondary-menu-mobile">
+                    <ul>
+                        <li class="mail"><i class="fa fa-envelope" aria-hidden="true"></i><a href="maito:geral@jf-pardilho.com">geral@jf-pardilho.com</a></li>
+                        <li class="phone"><i class="fa fa-phone" aria-hidden="true"></i>(+351) 234 287 211</li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+        <!--  END Header & Menu  -->
+            
+        <!--  Main Wrap  -->
+        <div id="main-wrap">
+            <!--  Page Content  -->
+            <div id="page-content" class="header-static">
+                <!--  Page Header  -->
+                <div id="page-header" class="secondary-background">
+                    <div class="container">
+                        <div class="row no-margin">
+                            <div class="text">
+                                <h1 class="white">Notícias.</h1>
+                                <ul class="breadcrumb white">
+                                    <li><a href="index.html">Home</a></li>
+                                    <li>Notícias</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!--  END Page Header  -->
+                <div id="home-wrap" class="content-section fullpage-wrap">
+                    <div class="container">
+                        <!-- News -->
+                        <!--  News Section  -->
+                        <section id="news" class="page">
+                            <div class="news-items equal three-columns">
+                                
+                                <!-- Single News -->
+
+                                <?php
+                                  // Include the database configuration file
+                                  include 'dbConfig.php';
+    
+                                   // Get records from the database
+                                   $query = $db->query("SELECT * FROM noticias ORDER BY data DESC");
+
+                                   if($query->num_rows > 0){ 
+                                    while($row = $query->fetch_assoc()){ 
+                                    $postID = $row['id'];
+                                    ?>
+
+                                <div class="single-news one-item"> 
+                                    <article>
+                                        <img src="noticias/noticias-img/<?php echo $row['imagem']; ?>" alt="">
+                                        <div class="content">
+                                            <h3><?php echo $row['titulo']; ?></h3>
+                                            <p><?php echo $row['texto']; ?></p>
+                                            <span class="date"><?php echo $row['data']; ?></span>
+                                        </div>
+                                        <a href="noticia.php?id=<?php echo $row['id']; ?>" class="link"></a>
+
+                                    </article>
+                                </div>
+                                <?php } ?>
+                                <?php } ?>
+
+                            </div>
+                            <!--<div class="news-items equal three-columns postList"></div>
+
+                            <div class="show_more_main" id="show_more_main<?php echo $postID; ?>">
+                                  <span id="<?php echo $postID; ?>" class="show_more" title="Load more posts">Show more</span>
+                                 <span class="loding" style="display: none;"><span class="loding_txt">Loading...</span></span>
+                            </div>-->
+                                
+
+                        </section>
+                        <!-- END News -->
+                        <!--  Navigation  -->
+                        <!--<section id="nav" class="padding-top-null">
+                            <div class="row">
+                                <div class="col-xs-6">
+                                    <div class="nav-left">
+                                        <a href="single-blog-style-2.html" class="btn-alt small shadow margin-null"><i class="icon ion-ios-arrow-left"></i><span>Mais antigo</span></a>
+                                    </div>
+                                </div>
+                                <div class="col-xs-6">
+                                    <div class="nav-right">
+                                        <a href="#" class="btn-alt small shadow margin-null"><span>Mais recente</span><i class="icon ion-ios-arrow-right"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </section>-->
+                        <!--  END Navigation  -->
+                    </div>
+                </div>
+            </div>
+            <!--  END Page Content -->
+        </div>
+        <!--  Main Wrap  -->
+        
+
+        <!--  Footer  -->
+        <footer>
+            <div class="container">
+                <div class="row no-margin">
+                    <div class="col-md-4 text">
+                        <div class="logo">
+                            <img src="assets/img/BrasaoPardilho_favIcon.png" class="normal" alt="logo">
+                            <img src="assets/img/logo_white@2x.png" class="retina" alt="logo">
+                        </div>
+                        <p>Telefone: (+351) 234 287 211<br>
+                           Email: geral@jf-pardilho.com</p>
+                        
+                    </div>
+                    <div class="col-md-3 text small">
+                        <h4 class="heading white margin-bottom-small weight-300 footer-title">Morada</h4>
+                        <p>Junta de Freguesia de Pardilhó<br>
+                            Rua Prof. Saavedra Guedes, nº 27<br>
+                            3860-437 Pardilhó<br>
+                            Portugal</a></p>
+                    </div>
+                    <div class="col-md-2 text small">
+                        <h4 class="heading white margin-bottom-small weight-300 footer-title">Links úteis</h4>
+                        <ul class="info">
+                            <li><a href="">Home</a></li>
+                            <li><a href="">FAQ</a></li>
+                            <li><a href="">Contactos</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-3 text">
+                        <h4 class="heading white margin-bottom-small weight-300 footer-title">Redes sociais</h4>
+                        <div>
+                            <ul class="social">
+                                <li><a href="https://www.facebook.com/Junta-de-Freguesia-de-Pardilh%C3%B3-397978673664395/" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                                <li><a href=""><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <div id="copy">
+            <div class="container">
+                <div class="row no-margin">
+                    <div class="col-md-6 text">
+                        <p>©2018 <a href="http://www.we-advisor.com/" target="_blank">WeAdvisor</a>. Todos os direitos reservados. </p>
+                    </div>
+                    <div class="col-md-6 text text-right">
+                        <ul>
+                            <li><a href="#">Politica de privacidade</a></li>
+                            <li><a href="#">Politica de Cookies</a></li>
+                            <li><a href="#">Termos e Condições</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--  END Footer. Class fixed for fixed footer  -->
+        
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="assets/js/jquery.min.js"></script>
+        <!-- All js library -->
+        <script src="assets/js/bootstrap/bootstrap.min.js"></script>
+        <script src="assets/js/jquery.flexslider-min.js"></script>
+        <script src="assets/js/owl.carousel.min.js"></script>
+        <script src="assets/js/isotope.min.js"></script>
+        <script src="assets/js/jquery.magnific-popup.min.js"></script>
+        <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&signed_in=false"></script>
+        <script src="assets/js/jquery.scrollTo.min.js"></script>
+        <script src="assets/js/smooth.scroll.min.js"></script>
+        <script src="assets/js/jquery.appear.js"></script>
+        <script src="assets/js/jquery.countTo.js"></script>
+        <script src="assets/js/jquery.scrolly.js"></script>
+        <script src="assets/js/plugins-scroll.js"></script>
+        <script src="assets/js/imagesloaded.min.js"></script>
+        <script src="assets/js/pace.min.js"></script>
+        <script src="assets/js/main.js"></script>
+        <script src="assets/js/show-hide-text.js"></script>
+
+        <script type="text/javascript">
+
+$(document).ready(function(){
+    $(document).on('click','.show_more',function(){
+        var ID = $(this).attr('id');
+        console.log(ID);
+        $('.show_more').hide();
+        $.ajax({
+            type:'POST',
+            url:'ajax_more.php',
+            data:'id='+ID,
+            success:function(html){
+                $('#show_more_main'+ID).remove();
+                $('.postList').append(html);
+            }
+        });
+    });
+});
+
+
+
+        </script>
+    </body>
+</html>
